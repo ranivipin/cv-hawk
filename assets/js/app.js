96abcb6833b547f77aeeab53d68cccb050125e93
ranivipin <vrani0066@gmail.com>
2023-11-09 15:48:52 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,80 @@
+function openNav() {
+  document.getElementById("navbar").classList.toggle("end-0");
+  document.body.classList.toggle("overflow-hidden");
+  document.getElementById("menubtn-icon").classList.toggle("cross");
+}
+$(".slider").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+$(".slick_slider").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  // autoplay: true,
+  arrow: true,
+  prevArrow: ".prev",
+  nextArrow: ".next",
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
